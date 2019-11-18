bd7a659f876ec841e7ddbe78dd372ad33126bf5e
alejandrio.vasay <alejandrio.vasay@tccd.edu>
2019-11-18 09:18:58 -0600

    Initial commit

---
+++ DirInfoCacher.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace GetRandomCaching
+{
+    public class DirInfoCacher
+    {
+        public DirInfoCacher()
+        {
+        }
+
+        public string GetRandom()
+        {
+            object cacheList = HttpRuntime.Cache.Get("biology") as List<string>;
+            string returnedString = string.Empty;
+
+            if (cacheList == null)
+            {
+                // Initialize our list
+                List<string> myList = new List<string>(new string[] { "biology-1", "biology-2", "biology-3" });
+
+                // Cache the 
+                HttpRuntime.Cache.Insert("biology", myList, null, DateTime.Now.AddMinutes(60d), System.Web.Caching.Cache.NoSlidingExpiration);
+
+                Random R = new Random();
+
+                // get random number from 0 to 2. 
+                int someRandomNumber = R.Next(0, myList.Count());
+                returnedString = myList.ElementAt(someRandomNumber);
+                return returnedString;
+            }
+            else
+            {
+                IList<string> objectCache = (IList<string>)cacheList;
+
+                Random R = new Random();
+
+                // get random number from 0 to 2. 
+                int someRandomNumber = R.Next(0, objectCache.Count());
+
+                returnedString = objectCache.ElementAt(someRandomNumber);
+
+                return returnedString;
+            }
+
+        }
+    }
+}
---
+++ Handler.ashx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GetRandomCaching
+{
+    /// <summary>
+    /// Summary description for Handler1
+    /// </summary>
+    public class Handler1 : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+           
+            DirInfoCacher infoCacher = new DirInfoCacher();
+
+            string randomEl = infoCacher.GetRandom();
+
+            context.Response.Write(randomEl);
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
